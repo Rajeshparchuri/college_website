f74a9cefaccb68aabccb4e091ca1df9f30ce28dc
Rajeshparchuri <144262094+Rajeshparchuri@users.noreply.github.com>
2024-03-20 17:59:11 +0530

    Add files via upload

---
+++ starter/js/app.js
@@ -0,0 +1,31 @@
+// DOM => document Object model
+
+function init() {
+    const hamburger = document.querySelector(".hamburger");
+    const sidebarCloseButton = document.querySelector(".close-button");
+    const sidebarContainer = document.querySelector(".sidebar-container");
+
+    const openSidebar = function () {
+      sidebarContainer.classList.add("show-sidebar");
+    };
+    const closeSidebar = function () {
+      sidebarContainer.classList.remove("show-sidebar");
+    };
+    hamburger.addEventListener("click", openSidebar);
+    sidebarCloseButton.addEventListener("click", closeSidebar);
+  };
+  init();
+  // sticky sidebar
+  (function () {
+    const navbarContainer = document.querySelector(".nav-container");
+    window.addEventListener("scroll", function () {
+      let targetNumber = this.innerWidth >= 992 ? 70 : 50;
+      if (this.scrollY >= targetNumber) {
+        navbarContainer.classList.add("sticky-nav");
+      } else {
+        navbarContainer.classList.remove("sticky-nav");
+      }
+    });
+  })();
+
+    
